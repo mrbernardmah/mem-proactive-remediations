9ed1f92fe399e98f48bbd3950544579d17b944ae
Mr B <iTranc3@gmail.com>
2025-05-22 08:09:23 +1000

    Add files via upload

---
+++ HPWolfSecurity Remediate Version 1/Detect.ps1
@@ -0,0 +1,20 @@
+$Apps = Get-CimInstance -ClassName Win32_Product | Where-Object {(($_.Name -like "HP Wolf*") `
+    -or ($_.Name -like "HP Security*") `
+    -or ($_.Name -like "HP Connection Optimizer*") `
+    -or ($_.Name -like "HP Documentation*") `
+    -or ($_.Name -like "HP Security Update Service*") `
+    -or ($_.Name -like "HP Sure Run Module*") `
+    -or ($_.Name -like "HP Easy Clean*") `
+    -or ($_.Name -like "HP Privacy Settings*") `
+    -or ($_.Name -like "HP QuickDrop*") `
+    -or ($_.Name -like "HP Support Assistant*") `
+    -or ($_.Name -like "HP Wolf Security - Console*") `
+    -or ($_.Name -like "HP System Information*") `
+    -or ($_.Name -like "HP WorkWell*") `
+    -or ($_.Name -like "myHP*"))} | Select-Object Name, IdentifyingNumber | Sort-Object Name -Descending
+if ($Apps.Count -eq 0) {
+    Exit 0
+}
+else {
+    Exit 1
+}
